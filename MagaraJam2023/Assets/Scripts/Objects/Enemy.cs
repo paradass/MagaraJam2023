2987b9e5bc918931b98f7bbacf3b3ce86bb4aa5b
Burak Görez <burakgorez01@gmail.com>
2023-11-16 21:29:44 +0300

    Agent

---
+++ MagaraJam2023/Assets/Scripts/Objects/Enemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public float Damage;
+
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Agent();
+    }
+
+    void Agent()
+    {
+        agent.destination = Player.Instance.transform.position;
+        agent.isStopped = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Player.Instance.Damage(Damage);
+            Destroy(gameObject);
+        }
+    }
+}
