f0f38f64b7061bc1bfc5a2cf7da5bd0c4600b5de
Burak Görez <burakgorez01@gmail.com>
2023-11-16 19:01:12 +0300

    Character and environment

---
+++ MagaraJam2023/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance;
+
+    public float Health, MovementSpeed, RotationSpeed;
+
+    Slider healthBar;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        healthBar = GameObject.Find("HealthBar").GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        Ui();
+        Movement();
+    }
+
+    void Ui()
+    {
+        healthBar.value = Health;
+    }
+
+    void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal")*RotationSpeed*Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical")* MovementSpeed * Time.deltaTime;
+
+        transform.Rotate(new Vector3(0, horizontal, 0));
+        transform.Translate(new Vector3(0, 0, vertical));
+    }
+}
